193ad1e2685ce00a51af6832aa628e6e206ba9ec
daveraskin <draskin1991@gmail.com>
2023-11-30 10:04:59 -0800

    add models

--- pythonbuilderapi/models.py
+++ pythonbuilderapi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Polygon(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    userName = models.CharField(max_length=100)
+    message = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    color = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.userName}'s Polygon"
